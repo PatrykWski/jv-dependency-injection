624dfb445a9e4355da6b1f036bed822d70a06220
PatrykWski <patrykwiszniewski96@gmail.com>
2026-03-30 19:00:45 +0200

    unused import

--- src/main/java/mate.academy/Main.java
+++ src/main/java/mate.academy/Main.java
@@ -3,7 +3,6 @@ package mate.academy;
 import java.util.List;
 import mate.academy.lib.Injector;
 import mate.academy.model.Product;
-import mate.academy.service.ProductService;
 import mate.academy.service.impl.ProductServiceImpl;
 
 public class Main {
